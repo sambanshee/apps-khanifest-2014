c0f287deac9cae200d55b257c731d969d33a8dea
sambanshee <banshee.sodar@gmail.com>
2015-02-20 15:27:53 +0200

    added basic editing, basic comments

--- apps-khanifest/bot.py
+++ apps-khanifest/bot.py
@@ -1,4 +1,4 @@
- # -*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
 import bottle
 import datetime
 from bottle import *
@@ -8,11 +8,18 @@ from google.appengine.ext import ndb
 app = Bottle()
 
 database_key = ndb.Key('Application', 'submitted_apps')
+comments_key = ndb.Key('Comments', 'comments')
 
 class Participant(ndb.Model):
     participant_fn = ndb.StringProperty(required=True)
     participant_nickname = ndb.StringProperty(required=True)
     participant_age = ndb.IntegerProperty(required=True)
+    
+class Comments(ndb.Model):
+    comment_date = ndb.DateTimeProperty(auto_now_add=True)
+    comment = ndb.TextProperty(indexed=False)
+    author = ndb.UserProperty(required=True)
+    application = ndb.StringProperty(required=True)
 
 class Application(ndb.Model):
     author = ndb.UserProperty(required=True)
@@ -35,8 +42,8 @@ def main_page():
     else:
         users.create_logout_url(request.url)
 
-    application_query = Application.query(ancestor=database_key).order(-Application.date) 
-    applications = application_query.fetch(10)
+    application_query = Application.query(Application.author == user).order(-Application.date) 
+    applications = application_query.fetch()
     
     output = template('main.html', content = applications)
     return output
@@ -78,6 +85,85 @@ def write_form():
     application.put()
     #return "%s" % participants_list  
     redirect("/")
+
+
+#not going to work, just adding stuff
+@app.route('/edit/<url_id>')
+def edit_post(url_id):
+    user = users.get_current_user()
+    if not user:
+        redirect(users.create_login_url(request.url))
+
+    apps = ndb.Key(urlsafe=url_id)
+    application = Application.query(ancestor=apps).fetch()
+    
+    if application[0].author != user and not users.is_current_user_admin():
+        redirect('/')          
+    
+    try:
+      comments = Comments.query(Comments.application == url_id).order(+Comments.comment_date).fetch()
+    except:
+      comments = ""
+    
+    output = template('app-edit.html', app = application[0], action="edit", comments=comments, url_id=url_id )
+
+    return output
+    
+
+#not going to work, just adding stuff
+@app.route('/edit', method='POST')
+def edit_post():
+    user = users.get_current_user()
+    if not user:
+        redirect(users.create_login_url(request.url))
+    
+    url_id = request.forms.get('url_id')
+    #return url_id
+    application = ndb.Key(urlsafe=url_id).get()
+
+    if application.author != user and not users.is_current_user_admin() :
+        redirect('/')  
+   
+    application.content = request.forms.get('content')
+    application.author_fn = request.forms.get('author_fn')
+    application.author_phone = request.forms.get('author_phone')
+    '''
+    application.author_bdate = datetime.strptime(request.forms.get('author_bdate'), '%d-%m-%Y')
+    application.author_mail = request.forms.get('author_mail')
+    application.author_contacts = request.forms.get('author_contacts')
+    application.app_type = request.forms.get('app_type').decode('utf-8')
+    '''    
+    
+    participants_list = []
+    
+    for i in range(10):
+      fullname = request.forms.get("participant_fn_%d" % i)
+      nickname = request.forms.get("participant_nickname_%d" % i)
+      age = request.forms.get("participant_age_%d" % i)
+      delete = request.forms.get("participant_delete_%d" % i)
+      #participants_list.append([fullname, nickname, age, "participant_fn_%d" % i])
+      if fullname and nickname and age and not delete:
+        try:
+          participants_list.append(Participant(participant_fn=fullname, 
+                                               participant_nickname=nickname,
+                                               participant_age=int(age) )
+                                   )
+        except:
+          redirect("/error")
+    #print participants_list
+    application.participants = participants_list 
+           
+    comment = Comments(parent=comments_key)
+    comment_body = request.forms.get('comment')
+    if comment_body:
+        comment.comment = request.forms.get('comment')
+        comment.author = user
+        comment.application = url_id
+        comment.put()       
+    application.put()
+    
+    redirect('/')    
+
 
 @app.route('/error')
 def error_page():  
